780d2831485736d2fdae4fd37bc1f66e7a58fc82
hbszjkcy <8722668@gmail.com>
2026-06-26 11:25:23 +0800

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -827,8 +827,6 @@ function getHTML() {
         <div class="header">
             <h1>🤖 CF AI Chat</h1>
             <p>支持多模型切换的智能聊天助手</p>
-            <div class="author-info" ('https://gpt.cmtd.indevs.in/')">
-               // <p>算例白嫖:<strong>MY:GPT</strong></p>
             </div>
         </div>
         <div class="main-content">
